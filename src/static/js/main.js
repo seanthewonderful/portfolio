c6ac0de046d0469b300f312c8fb2fc3f5a4bef4d
Sean Fagan <58955260+seanthewonderful@users.noreply.github.com>
2022-06-18 16:40:21 -0600

    content changes

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -145,8 +145,8 @@
     new Typed('.typed', {
       strings: typed_strings,
       loop: true,
-      typeSpeed: 100,
-      backSpeed: 50,
+      typeSpeed: 70,
+      backSpeed: 40,
       backDelay: 2000
     });
   }
